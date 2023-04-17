36a9d9c66e85aecc8de40d33247bf774eb10d548
Jacob Maier <jacobamaier@gmail.com>
2023-04-17 09:44:29 -0700

    Initial setup.
    Co-authored-by: johnedisc <christopher.johnedis@gmail.com>

---
+++ src/Components/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "./Navbar";
+import Sidebar from "./Sidebar";
+
+function App() {
+  return (
+    <React.Fragment>
+      <Navbar />
+      <Sidebar />
+    </React.Fragment>
+  );
+}
+
+export default App
