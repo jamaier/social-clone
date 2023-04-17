f0f88ea3dbf8426a13aea09ddb59221657f1b2a9
Jacob Maier <jacobamaier@gmail.com>
2023-04-17 10:52:38 -0700

    Update sidebar with user
    Co-authored-by: johnedisc <christopher.johnedis@gmail.com>

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -13,10 +13,14 @@ function Sidebar() {
 
 function Friend() {
   return (
-    <React.Fragment>
-      {/* <img src={friendAvatar} alt="this is a friend image" /> */}
-      <button>please like me</button>
-    </React.Fragment>
+    <>
+      <div class='friend'>
+        {/* <img src={friendAvatar} alt="this is a friend image" /> */}
+        <div class='friend-img'>this is a person</div>
+        <p>my-name</p>
+        <button>please like me</button>
+      </div>
+    </>
   );
 }
 
