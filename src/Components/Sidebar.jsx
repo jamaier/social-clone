d7c431a323c3ed4446839993a92bf0b3c0c01b4a
Jacob Maier <jacobamaier@gmail.com>
2023-04-17 10:28:16 -0700

    Update sidebar
    Co-authored-by: johnedisc <christopher.johnedis@gmail.com>

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -1,35 +1,21 @@
 import React from "react";
-import reactLogo from "./../assets/react.svg";
-import viteLogo from "/vite.svg";
 import { useState } from "react";
 import './../App.css'
+// import friendAvatar from "./img/avatar.PNG";
 
 function Sidebar() {
-  const [count, setCount] = useState(0);
   return (
     <React.Fragment>
-      <div className="App">
-        <div>
-          <a href="https://vitejs.dev" target="_blank">
-            <img src={viteLogo} className="logo" alt="Vite logo" />
-          </a>
-          <a href="https://reactjs.org" target="_blank">
-            <img src={reactLogo} className="logo react" alt="React logo" />
-          </a>
-        </div>
-        <h1>Vite + React</h1>
-        <div className="card">
-          <button onClick={() => setCount((count) => count + 1)}>
-            count is {count}
-          </button>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test HMR
-          </p>
-        </div>
-        <p className="read-the-docs">
-          Click on the Vite and React logos to learn more
-        </p>
-      </div>
+      <Friend />
+    </React.Fragment>
+  );
+}
+
+function Friend() {
+  return (
+    <React.Fragment>
+      {/* <img src={friendAvatar} alt="this is a friend image" /> */}
+      <button>please like me</button>
     </React.Fragment>
   );
 }
